f0fa1ca513deadf5ef1a76e5c37578ae6f7f5fa0
danyukezz <bordunovden2@gmail.com>
2024-05-22 16:54:21 +0200

    RPi

--- RPi/app.py
+++ RPi/app.py
@@ -6,8 +6,10 @@ from bluetooth_uart_server.bluetooth_uart_server import ble_gatt_uart_loop
 
 # extend this code so the value received via Bluetooth gets printed on the LCD
 # (maybe together with you Bluetooth device name or Bluetooth MAC?)
-
+from lcd import LCD
+lcd = LCD()
 def main():
+    lcd.lcd_init()
     i = 0
     rx_q = queue.Queue()
     tx_q = queue.Queue()
@@ -18,9 +20,10 @@ def main():
             incoming = rx_q.get(timeout=1) # Wait for up to 1 second 
             if incoming:
                 print("In main loop: {}".format(incoming))
+                lcd.send_string(f"Drones: {incoming}", 0x80)
         except Exception as e:
             pass # nothing in Q 
-
+            lcd.clear()
         # if i%5 == 0: # Send some data every 5 iterations
         #     tx_q.put("test{}".format(i))
         # i += 1
